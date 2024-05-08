883885bbfaece9c996647a2bb3685036ba210017
Stefan Dimitrov <115184100+StefanDimitrov04@users.noreply.github.com>
2024-05-08 14:20:24 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const searchButton = document.querySelector('.search-btn');
-const cityName = document.querySelector('.city-input');
+const cityNameInput = document.querySelector('.city-input');
 const divWeatherCard = document.querySelector('.weather-cards');
 const currentWeatherDiv = document.querySelector('.current-weather');
 const currentLocationButton = document.querySelector('.location-btn');
@@ -10,8 +10,7 @@ const errorContainer = document.querySelector('.error-container');
 const errorMessage = document.querySelector('.error-message');
 
 //layouts for current weather and for the next 6 days
-createCardForDay = (cityName, cardForDay, index) => {
-
+const createCardForDay = (cityName, cardForDay, index) => {
   if(index === 0) {
     return `
     <div class="details">
@@ -21,22 +20,22 @@ createCardForDay = (cityName, cardForDay, index) => {
     <h6>Humidity: ${cardForDay.main.humidity}%</h6>
   </div>
   <div class="icon">
-  <img src="https://openweathermap.org/img/wn/${cardForDay.weather[0].icon}@4x.png" alt="weather-icon">
+  <img src="https://openweathermap.org/img/wn/${cardForDay.weather[0].icon}.png" alt="weather-icon">
   <h4>${cardForDay.weather[0].description}</h4>
 </div>
-    `
+    `;
   } else {
    return `
     <li class="card">
     <h3>${cardForDay.dt_txt.split(" ")[0]}</h3>
-    <img src="https://openweathermap.org/img/wn/${cardForDay.weather[0].icon}@4x.png" alt="weather-icon">
+    <img src="https://openweathermap.org/img/wn/${cardForDay.weather[0].icon}.png" alt="weather-icon">
     <h6>Temp:${(Number(cardForDay.main.temp) - 273.15).toFixed(2)}°C</h6>
     <h6>Wind: ${cardForDay.wind.speed} M/S</h6>
     <h6>Humidity: ${cardForDay.main.humidity}%</h6>
   </li>
-    `
-}
-}
+    `;
+  }
+};
 
 //getting city from the value of the search input
 async function getCity(city) {
@@ -44,89 +43,88 @@ async function getCity(city) {
     const currentCity = city.trim();
     if(!currentCity) return;
 
-   const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKEY}`;
-  const response = await fetch(apiUrl);
-  let data = await response.json();
-const {lat, lon, name } = data[0];
-getWeatherDetails(lat, lon, name);
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKEY}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const {lat, lon, name } = data[0];
+    getWeatherDetails(lat, lon, name);
   } catch (error) {
     errorMessage.textContent = "Invalid city name!";
     errorContainer.style.display = 'block';
-    cityName.value = "";
+    cityNameInput.value = "";
     setTimeout(function() {
-     errorContainer.style.display = "none";
-   }, 2000);
-  }
-   
+      errorContainer.style.display = "none";
+    }, 2000);
+  } 
 }
 
 //getting the details of the weather from the api response
 async function getWeatherDetails(lat, lon, cityName) {
   try {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKEY}`;
-   const response = await fetch(apiUrl);
-   const data = await response.json();
-   
-   const newForecastDates = [];
-   const fiveDaysForecast = data.list.filter(forecast => {
-    const dateForecast = new Date(forecast.dt_txt).getDate();
-    if(!newForecastDates.includes(dateForecast)) {
-        return newForecastDates.push(dateForecast);
-    }
-   })
-    cityName.value = "";
-   divWeatherCard.innerHTML = "";
-  currentWeatherDiv.innerHTML = " ";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-   fiveDaysForecast.forEach( (weatherDay, index) => {
-    if(index == 0) {
-      currentWeatherDiv.insertAdjacentHTML('beforeend',createCardForDay(cityName,weatherDay, index))
-    } else {
-     
-      divWeatherCard.insertAdjacentHTML('beforeend',createCardForDay(cityName,weatherDay, index));
-    }
+    const newForecastDates = [];
+    const fiveDaysForecast = data.list.filter(forecast => {
+      const dateForecast = new Date(forecast.dt_txt).getDate();
+      if(!newForecastDates.includes(dateForecast)) {
+          return newForecastDates.push(dateForecast);
+      }
+    });
+    
+    cityNameInput.value = "";
 
-   })
+    divWeatherCard.innerHTML = "";
+    currentWeatherDiv.innerHTML = "";
+
+    fiveDaysForecast.forEach((weatherDay, index) => {
+      if(index === 0) {
+        currentWeatherDiv.insertAdjacentHTML('beforeend', createCardForDay(cityName, weatherDay, index))
+      } else {
+        divWeatherCard.insertAdjacentHTML('beforeend', createCardForDay(cityName, weatherDay, index));
+      }
+    });
   } catch (error) {
     errorMessage.textContent = error.message;
     errorContainer.style.display = 'block';
     setTimeout(function() {
-     errorContainer.style.display = "none";
-   }, 2000);
+      errorContainer.style.display = "none";
+    }, 2000);
   }
 
 }
 
-
-//get userCoordinates function
 const getUserCoords = () => {
   navigator.geolocation.getCurrentPosition(
     position => {
-         const {latitude, longitude} = position.coords;
-         console.log(latitude, longitude);
-         const reverseGeocodingURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKEY}`
-         
-         fetch(reverseGeocodingURL).then(res => res.json()).then(data => {
+      const {latitude, longitude} = position.coords;
+      console.log(latitude, longitude);
+      const reverseGeocodingURL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKEY}`;
+      
+      fetch(reverseGeocodingURL)
+        .then(res => res.json())
+        .then(data => {
           const  name = data[0].name;
           console.log(name);
           getWeatherDetails(latitude, longitude, name);
-         });
+        });
 
     },
     error =>  {
-     errorMessage.textContent = error.message;
-     errorContainer.style.display = 'block';
-     setTimeout(function() {
-      errorContainer.style.display = "none";
-    }, 2000);
+      errorMessage.textContent = error.message;
+      errorContainer.style.display = 'block';
+      setTimeout(function() {
+        errorContainer.style.display = "none";
+      }, 2000);
     }
-  )
+  );
 }
 
 searchButton.addEventListener('click', () => {
-    getCity(cityName.value);
+  getCity(cityNameInput.value);
 });
 
 currentLocationButton.addEventListener('click', () => {
   getUserCoords();
-})
+});
